9ff4afc836743a3c44a776c09e92550870f22c45
simon.meng <simon.meng@foxmail.com>
2024-12-28 13:47:09 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import SqlEditor from './components/SqlEditor';
+import SqlEditor from './components/CodeEditor.tsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -31,7 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
+    </div>
   )
 }
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    console.log(`Vite mode: ${mode}`); // Debugging or logging
+  return {
+    plugins: [react()],
+    base: './',
+  };
+});
