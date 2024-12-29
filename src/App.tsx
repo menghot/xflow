e5abe1862ac9fa6e3720b4d62021c5f08b01afd6
simon.meng <simon.meng@foxmail.com>
2024-12-29 13:23:43 +0800

    bpmn able to run

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import SqlEditor from './components/MonacoEditor.tsx';
-import CodeEditor from './components/CodeMirrorEditor.tsx';
-import Editors from './components/MultipleCodeEditors.tsx';
+// src/App.tsx
+import React from "react";
+import BpmnEditorComponent from "./components/BpmnModel.tsx";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
     <div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-
-      <div className="card">
-          <SqlEditor/>
-          <CodeEditor/>
-          <Editors/>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Workflow Pipeline</h1>
+      <BpmnEditorComponent />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
