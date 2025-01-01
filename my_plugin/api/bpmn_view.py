95231ddf02ad4bb7bb9df1a667058ba92f1d4abf
simon.meng <simon.meng@foxmail.com>
2025-01-01 20:53:38 +0800

    update

--- my_plugin/api/bpmn_view.py
+++ my_plugin/api/bpmn_view.py
@@ -1,5 +1,4 @@
 import os
-from urllib.parse import unquote
 
 from airflow.www.app import csrf
 from flask import Blueprint, request, jsonify
@@ -44,8 +43,8 @@ def deploy():
         transformer = BPMNToAirflowTransformer(None, bpmn_xml)
         dag_code = transformer.generate_airflow_dag()
 
-        # save to airflow home
-        output_file = os.path.join(os.path.dirname(__file__), "../../dags", transformer.process_id + ".py")
+        # Save DAG to $AIRFLOW_HOME/dags by default
+        output_file = os.path.join(os.path.dirname(__file__), "../../../dags", transformer.process_id + ".py")
 
         with open(output_file, "w") as f:
             f.write(dag_code)
