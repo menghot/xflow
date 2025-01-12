c031d56157fd15856636ff9aaac50ca23f3f36fc
simon.meng <simon.meng@foxmail.com>
2025-01-12 12:24:14 +0800

    update

--- src/custom/CustomizeReplaceMenuProvider.js
+++ src/custom/CustomizeReplaceMenuProvider.js
@@ -1,38 +1,39 @@
 export default class MyReplaceMenuProvider {
 
-  constructor(replaceMenuProvider, popupMenu) {
-    this.replaceMenuProvider = replaceMenuProvider;
-    this.popupMenu = popupMenu;
-    console.log('---replaceMenuProvider---',replaceMenuProvider);
-    replaceMenuProvider.getEntries = function(target) {
-      return [];
-    };
-    replaceMenuProvider.getPopupMenuEntries = function(target) {
-      return [];
-    };
+    constructor(replaceMenuProvider, popupMenu) {
+        this.replaceMenuProvider = replaceMenuProvider;
+        this.popupMenu = popupMenu;
+        console.log('---replaceMenuProvider---', replaceMenuProvider);
+
+        // Overwrite/Remove the injected provider
+        replaceMenuProvider.getEntries = function(target) {
+          return [];
+        };
+        replaceMenuProvider.getPopupMenuEntries = function(target) {
+          return [];
+        };
+
+        popupMenu.registerProvider('bpmn-replace', 2000, this);
+        console.log('---replaceMenuProvider---', replaceMenuProvider);
+    }
 
 
-    popupMenu.registerProvider('bpmn-replace', 2000, this);
-    console.log('---replaceMenuProvider---',replaceMenuProvider);
-  }
-
-
-  getPopupMenuEntries(target) {
-    console.log(target , '---MyReplaceMenuProvider----1----');
-    return [
-      {
-        label: 'Custom Task',
-        actionName: 'replace-with-custom-task',
-        className: 'bpmn-icon-custom-task',
-        target: {
-          type: 'bpmn:ServiceTask',
-        },
-      },
-    ];
-  }
+    getPopupMenuEntries(target) {
+        console.log(target, '---MyReplaceMenuProvider----1----');
+        return this.replaceMenuProvider._createEntries(target, [
+            {
+                label: 'Custom Task',
+                actionName: 'replace-with-custom-task',
+                className: 'bpmn-icon-custom-task',
+                target: {
+                    type: 'bpmn:ServiceTask',
+                },
+            },
+        ]);
+    }
 }
 
 MyReplaceMenuProvider.$inject = [
-  'replaceMenuProvider',
-  'popupMenu'
+    'replaceMenuProvider',
+    'popupMenu'
 ];
