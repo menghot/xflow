c73a975342eae919ccf6c2c61949cba183472fa8
simon.meng <simon.meng@foxmail.com>
2025-03-16 17:40:46 +0800

    support multiple sql execute

--- xcodeflow/bpmn_to_dag_transformer.py
+++ xcodeflow/bpmn_to_dag_transformer.py
@@ -81,9 +81,15 @@ from xcodeflow.task.sql_task import execute_sql
 def sample_task(task_name):
     print(f"Executing: {{task_name}}")
 
+
 def execute_sql(connection, sql):
     print(f"Executing: {{connection}}, {{sql}}")
     execute_sql(connection, sql)
+    
+    
+def fetch_nothing(cursor):
+    print(cursor)
+
 
 default_args = {{
     'start_date': datetime(2023, 1, 1),
@@ -109,11 +115,8 @@ with DAG(dag_id='{self.process_id}', default_args=default_args, schedule_interva
             task_id_var = f"t_{english_letters}_{task_id.lower().replace('activity_', '')}"
 
             if "sql" in item["magic"] and "spark" not in item["magic"]["connection"]:
-                task_code = f"""    {task_id_var} = SQLExecuteQueryOperator(conn_id='{item["magic"]["connection"]}', task_id='{task_name}',sql=f\"""{item["magic"]["sql"]}\""")\n"""
+                task_code = f"""    {task_id_var} = SQLExecuteQueryOperator(conn_id='{item["magic"]["connection"]}', split_statements=True, handler=fetch_nothing, task_id='{task_name}',sql=f\"""{item["magic"]["sql"]}\""")\n"""
                 dag_code += task_code
-            # if "sql" in item["magic"]:
-            #     task_code = f"""    {task_id_var} = PythonOperator(task_id='{task_name}', python_callable=execute_sql, op_args=['{item["magic"]["connection"]}', f\"""{item["magic"]["sql"]}\"""])\n"""
-            #     dag_code += task_code
             elif "script" in item["magic"]:
                 dag_code += item["magic"]["script"]
             else:
